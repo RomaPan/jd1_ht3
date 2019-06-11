4f9db050f68b14dee6ee268df4094faae8443a2b
RomaPan <48756025+RomaPan@users.noreply.github.com>
2019-06-11 19:12:56 +0300

    new commit

--- jd-11-Treasures/src/by/htp/treasure/entity/Treasure.java
+++ jd-11-Treasures/src/by/htp/treasure/entity/Treasure.java
@@ -12,4 +12,49 @@ public class Treasure {
 	public double getPrice() {
 		return price;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Treasure other = (Treasure) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Treasure [name=" + getName() + ", price=" + getPrice() + "]";
+	}
+	
+	
 }
